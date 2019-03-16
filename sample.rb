1a768a2c0138df7a50f39c25b67530bfa82875de
Eishi Tominaga <eishi605@gmail.com>
2019-03-16 09:14:35 +0900

    Add lucky object

--- sample.rb
+++ sample.rb
@@ -1,1 +1,12 @@
-aaa
+require 'dxruby'
+
+image = Image.load('resources/images/lucky.png')
+
+Window.loop do
+  if Input.key_down?(K_A) then
+    Window.draw_scale(100, 100, image, 4, 4)
+  else
+    Window.draw_scale(0, 0, image, 4, 4)
+  end
+end
+
