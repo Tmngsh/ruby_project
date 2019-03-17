be437a19dbc7ee6c637333ca19928a09b0fea311
Eishi Tominaga <eishi605@gmail.com>
2019-03-17 11:11:12 +0900

    迷路自動生成、木の画像を追加

--- sample.rb
+++ sample.rb
@@ -1,12 +1,108 @@
 require 'dxruby'
 
 image = Image.load('resources/images/lucky.png')
+image_tree = Image.load('resources/images/tree_with_background.png')
+
+scale = 1;
+
+
+# 迷路作成(top:1, right:2, bottom:3, left:4)
+# WALL=0, FLOOR=1
+WALL = 0
+FLOOR = 1
+WIDTH = 31
+HEIGHT = 21
+
+a = Array.new(WIDTH*HEIGHT)
+
+for i in 0 .. HEIGHT - 1 do
+  for j in 0 .. WIDTH - 1 do
+    if (i%2 == 1) && (j%2 == 1) then
+      a[i*WIDTH + j] = WALL
+    else
+      a[i*WIDTH + j] = FLOOR
+    end
+  end
+end
+
+#迷路自動作成
+#一段目
+i = 1
+for j in 0 .. WIDTH - 1 do
+  if j%2 == 1 then
+    loop do
+      rand_num = rand(1..4)
+      if rand_num == 1 then # 下
+        if a[(i-1)*WIDTH + j] != WALL then
+          a[(i-1)*WIDTH + j] = WALL
+          break
+        end
+      elsif rand_num == 2 then # 右
+        if a[i*WIDTH + j+1] != WALL then
+          a[i*WIDTH + j+1] = WALL
+          break
+        end
+      elsif rand_num == 3 then # 下
+        if a[(i+1)*WIDTH + j] != WALL then
+          a[(i+1)*WIDTH + j] = WALL
+          break
+        end
+      else # 左
+        if a[i*WIDTH + j-1] != WALL then
+          a[i*WIDTH + j-1] = WALL
+          break
+        end
+      end
+    end
+  end
+end
+
+
+#2段目以降
+for i in 1 .. HEIGHT - 1 do
+  if i%2 == 1 then
+    for j in 0 .. WIDTH - 1 do
+      if j%2 == 1 then
+        loop do
+          rand_num = rand(2..4)
+          print rand_num, "\n"
+          if rand_num == 2 then # 右
+            if a[i*WIDTH + j+1] != WALL then
+              a[i*WIDTH + j+1] = WALL
+              break
+            end
+          elsif rand_num == 3 then # 下
+            if a[(i+1)*WIDTH + j] != WALL then
+              a[(i+1)*WIDTH + j] = WALL
+              break
+            end
+          else # 左
+            if a[i*WIDTH + j-1] != WALL then
+              a[i*WIDTH + j-1] = WALL
+              break
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
 
 Window.loop do
+  for i in 0 .. HEIGHT - 1 do
+    for j in 0 .. WIDTH - 1do
+      if a[i*WIDTH + j] == WALL then
+        Window.draw(j * 20, i * 20, image_tree);
+      else
+      end
+    end
+  end
   if Input.key_down?(K_A) then
-    Window.draw_scale(100, 100, image, 4, 4)
+    Window.draw(20, 20, image)
   else
-    Window.draw_scale(0, 0, image, 4, 4)
+    Window.draw(0, 0, image)
+    scale = 1;
   end
 end
 
